ce5592b44d45c8ca26b527e7321ee08537f1f27c
victormac1 <victorcognito@yahoo.com>
2021-11-11 01:53:11 +0100

    first commit

---
+++ Flying Bird/Flying_BirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flying_BirdApp.swift
+//  Flying Bird
+//
+//  Created by Victor Mac on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Flying_BirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
